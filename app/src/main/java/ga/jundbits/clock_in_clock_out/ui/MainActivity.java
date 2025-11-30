6035203733d3402d0de491fb3bbaf5f8cfa7a592
Georges Pharaon <99219167+georges-ph@users.noreply.github.com>
2025-11-30 13:00:55 +0200

    moved import from main to profiles
    and added icons to in and out buttons

--- app/src/main/java/ga/jundbits/clock_in_clock_out/ui/MainActivity.java
+++ app/src/main/java/ga/jundbits/clock_in_clock_out/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package ga.jundbits.clock_in_clock_out.ui;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -9,10 +8,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
@@ -31,12 +27,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.JsonSyntaxException;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import ga.jundbits.clock_in_clock_out.AppDatabase;
 import ga.jundbits.clock_in_clock_out.R;
 import ga.jundbits.clock_in_clock_out.Utils;
 import ga.jundbits.clock_in_clock_out.enums.Clocking;
@@ -45,7 +35,6 @@ import ga.jundbits.clock_in_clock_out.models.Profile;
 public class MainActivity extends AppCompatActivity {
 
     private ConstraintLayout mainLayout;
-    private ProgressBar mainProgressBar;
     private FloatingActionButton mainScanFab;
     private Button mainInButton, mainOutButton;
 
@@ -67,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void initVars() {
         mainLayout = findViewById(R.id.main_layout);
-        mainProgressBar = findViewById(R.id.main_progress_bar);
         mainScanFab = findViewById(R.id.main_scan_fab);
         mainInButton = findViewById(R.id.main_in_button);
         mainOutButton = findViewById(R.id.main_out_button);
@@ -158,15 +146,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(codeScannerIntent);
     }
 
-    private void importProfiles() {
-        // Select file from internal storage
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("*/*");
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"text/csv", "text/comma-separated-values", "text/plain", "application/csv"});
-        startActivityResultLauncher.launch(intent);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
@@ -175,10 +154,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.main_menu_import) {
-            importProfiles();
-            return true;
-        } else if (item.getItemId() == R.id.main_menu_profiles) {
+        if (item.getItemId() == R.id.main_menu_profiles) {
             startActivity(new Intent(this, ProfilesActivity.class));
             return true;
         } else if (item.getItemId() == R.id.main_menu_attendance) {
@@ -186,121 +162,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
-    }
-
-    private final ActivityResultLauncher<Intent> startActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-        if (result.getResultCode() != Activity.RESULT_OK || result.getData() == null || result.getData().getData() == null) {
-            Snackbar.make(mainLayout, R.string.no_file_selected, Snackbar.LENGTH_SHORT).show();
-            return;
-        }
-        processFile(result.getData().getData());
-    });
-
-    private void processFile(Uri fileUri) {
-        // Check if file extension is .csv
-        if (!Utils.isCSV(this, fileUri)) {
-            Snackbar.make(mainLayout, R.string.not_csv_file, Snackbar.LENGTH_SHORT).show();
-            return;
-        }
-
-        // Work in background thread to prevent blocking the UI (main thread)
-        new Thread(() -> {
-            try {
-                // Read the file
-                List<String> content = Utils.readFile(MainActivity.this, fileUri);
-
-                // Check if the file is completely empty or if there is only the headers row
-                if (content.isEmpty() || content.size() == 1) {
-                    runOnUiThread(() -> Snackbar.make(mainLayout, R.string.file_empty, Snackbar.LENGTH_SHORT).show());
-                    return;
-                }
-
-                // Read first line to check if it has CSV content and if columns are the columns needed
-                String[] requiredColumns = {"id", "name", "department"};
-                String[] columns = content.get(0).split(",");
-                if (columns.length != requiredColumns.length || !Arrays.equals(columns, requiredColumns)) {
-                    runOnUiThread(() -> Snackbar.make(mainLayout, R.string.not_csv_file, Snackbar.LENGTH_SHORT).show());
-                    return;
-                }
-
-                // Remove headers row
-                content.remove(0);
-                // Start inserting into database
-                insertToDB(content);
-            } catch (IOException e) {
-                runOnUiThread(() -> Snackbar.make(mainLayout, R.string.error_selecting_file, Snackbar.LENGTH_SHORT).show());
-            } catch (Exception e) {
-                runOnUiThread(() -> Snackbar.make(mainLayout, e.getMessage() != null ? e.getMessage() : getString(R.string.something_went_wrong), Snackbar.LENGTH_SHORT).show());
-            }
-        }).start();
-    }
-
-    private void insertToDB(List<String> lines) {
-        // Show the progress bar
-        runOnUiThread(() -> {
-            // Setup progress bar
-            mainProgressBar.setVisibility(View.VISIBLE);
-            mainProgressBar.setMax(lines.size());
-            mainProgressBar.setProgress(0);
-            // Disable buttons to prevent messing with the import
-            mainInButton.setEnabled(false);
-            mainOutButton.setEnabled(false);
-            mainScanFab.setEnabled(false);
-        });
-
-        try {
-            List<Profile> profiles = new ArrayList<>();
-
-            // Iterate through each line in the file
-            for (String line : lines) {
-                // Skip empty lines
-                if (line.isBlank()) {
-                    // Add 2 to the index instead of 1. List is zero based and the headers line is removed
-                    runOnUiThread(() -> Toast.makeText(this, getString(R.string.invalid_profile_with_line, lines.indexOf(line) + 2), Toast.LENGTH_SHORT).show());
-                    continue;
-                }
-
-                // Split line to get columns
-                String[] parts = line.split(",");
-
-                // Skip lines that don't have 3 columns and skip any entry that has an empty column
-                if (parts.length != 3 || parts[0].isBlank() || parts[1].isBlank() || parts[2].isBlank()) {
-                    // Add 2 to the index instead of 1. List is zero based and the headers line is removed
-                    runOnUiThread(() -> Toast.makeText(this, getString(R.string.invalid_profile_with_line, lines.indexOf(line) + 2), Toast.LENGTH_SHORT).show());
-                    continue;
-                }
-
-                // Map to profile object and add to list
-                Profile profile = new Profile(Integer.parseInt(parts[0].trim()), parts[1].trim(), parts[2].trim());
-                profiles.add(profile);
-
-                // Update progress bar to current profiles list size
-                runOnUiThread(() -> mainProgressBar.setProgress(profiles.size()));
-            }
-
-            AppDatabase.getInstance(this).runInTransaction(() -> {
-                // Empty profile table
-                AppDatabase.getInstance(getApplicationContext()).profileDao().deleteAll();
-                // Insert list into database
-                AppDatabase.getInstance(MainActivity.this).profileDao().insertAll(profiles);
-            });
-
-            // Show success message
-            runOnUiThread(() -> Snackbar.make(mainLayout, getString(R.string.import_message, profiles.size(), lines.size()), Snackbar.LENGTH_SHORT).show());
-        } catch (NumberFormatException e) {
-            runOnUiThread(() -> Snackbar.make(mainLayout, R.string.error_parsing_numbers, Snackbar.LENGTH_SHORT).show());
-        } catch (Exception e) {
-            runOnUiThread(() -> Snackbar.make(mainLayout, R.string.error_importing_profiles, Snackbar.LENGTH_SHORT).show());
-        } finally {
-            runOnUiThread(() -> {
-                // Hide progress bar
-                mainProgressBar.setVisibility(View.GONE);
-                // Reset buttons state
-                mainInButton.setEnabled(true);
-                mainOutButton.setEnabled(true);
-                mainScanFab.setEnabled(true);
-            });
-        }
     }
 
 }
--- app/src/main/java/ga/jundbits/clock_in_clock_out/ui/ProfilesActivity.java
+++ app/src/main/java/ga/jundbits/clock_in_clock_out/ui/ProfilesActivity.java
@@ -1,37 +1,50 @@
 package ga.jundbits.clock_in_clock_out.ui;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import ga.jundbits.clock_in_clock_out.AppDatabase;
 import ga.jundbits.clock_in_clock_out.R;
+import ga.jundbits.clock_in_clock_out.Utils;
 import ga.jundbits.clock_in_clock_out.enums.Clocking;
 import ga.jundbits.clock_in_clock_out.models.Profile;
 
 public class ProfilesActivity extends AppCompatActivity {
 
+    private ConstraintLayout profilesLayout;
+    private ProgressBar profilesProgressBar;
     private TextView profilesNoProfiles;
     private ListView profilesListView;
 
@@ -61,6 +74,8 @@ public class ProfilesActivity extends AppCompatActivity {
     }
 
     private void initVars() {
+        profilesLayout = findViewById(R.id.profiles_layout);
+        profilesProgressBar = findViewById(R.id.profiles_progress_bar);
         profilesNoProfiles = findViewById(R.id.profiles_no_profiles);
         profilesListView = findViewById(R.id.profiles_list_view);
     }
@@ -154,8 +169,125 @@ public class ProfilesActivity extends AppCompatActivity {
         if (item.getItemId() == android.R.id.home) {
             finish();
             return true;
+        } else if (item.getItemId() == R.id.profiles_menu_import) {
+            importProfiles();
+            return true;
         }
         return false;
+    }
+
+    private void importProfiles() {
+        // Select file from internal storage
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"text/csv", "text/comma-separated-values", "text/plain", "application/csv"});
+        startActivityResultLauncher.launch(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> startActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() != Activity.RESULT_OK || result.getData() == null || result.getData().getData() == null) {
+            Snackbar.make(profilesLayout, R.string.no_file_selected, Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+        processFile(result.getData().getData());
+    });
+
+    private void processFile(Uri fileUri) {
+        // Check if file extension is .csv
+        if (!Utils.isCSV(this, fileUri)) {
+            Snackbar.make(profilesLayout, R.string.not_csv_file, Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Work in background thread to prevent blocking the UI (main thread)
+        new Thread(() -> {
+            try {
+                // Read the file
+                List<String> content = Utils.readFile(ProfilesActivity.this, fileUri);
+
+                // Check if the file is completely empty or if there is only the headers row
+                if (content.isEmpty() || content.size() == 1) {
+                    runOnUiThread(() -> Snackbar.make(profilesLayout, R.string.file_empty, Snackbar.LENGTH_SHORT).show());
+                    return;
+                }
+
+                // Read first line to check if it has CSV content and if columns are the columns needed
+                String[] requiredColumns = {"id", "name", "department"};
+                String[] columns = content.get(0).split(",");
+                if (columns.length != requiredColumns.length || !Arrays.equals(columns, requiredColumns)) {
+                    runOnUiThread(() -> Snackbar.make(profilesLayout, R.string.not_csv_file, Snackbar.LENGTH_SHORT).show());
+                    return;
+                }
+
+                // Remove headers row
+                content.remove(0);
+                // Start inserting into database
+                insertToDB(content);
+            } catch (IOException e) {
+                runOnUiThread(() -> Snackbar.make(profilesLayout, R.string.error_selecting_file, Snackbar.LENGTH_SHORT).show());
+            } catch (Exception e) {
+                runOnUiThread(() -> Snackbar.make(profilesLayout, e.getMessage() != null ? e.getMessage() : getString(R.string.something_went_wrong), Snackbar.LENGTH_SHORT).show());
+            }
+        }).start();
+    }
+
+    private void insertToDB(List<String> lines) {
+        // Show the progress bar
+        runOnUiThread(() -> {
+            // Setup progress bar
+            profilesProgressBar.setVisibility(View.VISIBLE);
+            profilesProgressBar.setMax(lines.size());
+            profilesProgressBar.setProgress(0);
+        });
+
+        try {
+            List<Profile> profiles = new ArrayList<>();
+
+            // Iterate through each line in the file
+            for (String line : lines) {
+                // Skip empty lines
+                if (line.isBlank()) {
+                    // Add 2 to the index instead of 1. List is zero based and the headers line is removed
+                    runOnUiThread(() -> Toast.makeText(this, getString(R.string.invalid_profile_with_line, lines.indexOf(line) + 2), Toast.LENGTH_SHORT).show());
+                    continue;
+                }
+
+                // Split line to get columns
+                String[] parts = line.split(",");
+
+                // Skip lines that don't have 3 columns and skip any entry that has an empty column
+                if (parts.length != 3 || parts[0].isBlank() || parts[1].isBlank() || parts[2].isBlank()) {
+                    // Add 2 to the index instead of 1. List is zero based and the headers line is removed
+                    runOnUiThread(() -> Toast.makeText(this, getString(R.string.invalid_profile_with_line, lines.indexOf(line) + 2), Toast.LENGTH_SHORT).show());
+                    continue;
+                }
+
+                // Map to profile object and add to list
+                Profile profile = new Profile(Integer.parseInt(parts[0].trim()), parts[1].trim(), parts[2].trim());
+                profiles.add(profile);
+
+                // Update progress bar to current profiles list size
+                runOnUiThread(() -> profilesProgressBar.setProgress(profiles.size()));
+            }
+
+            AppDatabase.getInstance(this).runInTransaction(() -> {
+                // Empty profile table
+                AppDatabase.getInstance(getApplicationContext()).profileDao().deleteAll();
+                // Insert list into database
+                AppDatabase.getInstance(ProfilesActivity.this).profileDao().insertAll(profiles);
+            });
+
+            // Show success message
+            runOnUiThread(() -> Snackbar.make(profilesLayout, getString(R.string.import_message, profiles.size(), lines.size()), Snackbar.LENGTH_SHORT).show());
+        } catch (NumberFormatException e) {
+            runOnUiThread(() -> Snackbar.make(profilesLayout, R.string.error_parsing_numbers, Snackbar.LENGTH_SHORT).show());
+        } catch (Exception e) {
+            runOnUiThread(() -> Snackbar.make(profilesLayout, R.string.error_importing_profiles, Snackbar.LENGTH_SHORT).show());
+        } finally {
+            // Hide progress bar
+            runOnUiThread(() -> profilesProgressBar.setVisibility(View.GONE));
+        }
     }
 
 }
